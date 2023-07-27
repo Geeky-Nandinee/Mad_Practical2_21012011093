ec8cc7d07919a6c232a14ca32a1f6ae394089c5d
student <student@gnu.ac.in>
2023-07-27 08:53:15 +0530

    Practical_2

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Mad_Practical2_21012011093"
+include(":app")
